6708f6e7693f4864a9ca3ab000a27bd43a65f9d5
jaya-shankar <bjayashankar2102@gmail.com>
2023-09-06 19:06:14 +0200

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,202 @@
+# pyright: reportMissingModuleSource=false
+# pyright: reportMissingImports=false
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from utils import (
+    get_country_coords,
+    save_csv,
+)
+
+# dropdown box for selecting country
+
+st.title("Plots 🌎")
+
+le_df = pd.read_csv("./edu_datasets/life_expectancy_years.csv")
+
+countries = le_df["Country"].unique()
+
+edu_indices = [
+    "Primary Education",
+    "Lower Secondary Education",
+    "Higher Secondary Education",
+    "College Completion",
+]
+
+health_indices = [
+    "Total Fertility Rate",
+    "Life Expectancy",
+]
+
+econ_indices = [
+    "GDP per Capita",
+]
+
+time_indices = [
+    "Years",
+]
+
+indices = edu_indices + health_indices + econ_indices + time_indices
+
+
+cleaned_indices ={
+    "f_pri_edu" : "Female Primary Education",
+    "f_ls_edu" : "Female Lower Secondary Education",
+    "f_hs_edu" : "Female Higher Secondary Education",
+    "f_clg_comp" : "Female College Completion",
+    "m_pri_edu" : "Male Primary Education",
+    "m_ls_edu" : "Male Lower Secondary Education",
+    "m_hs_edu" : "Male Higher Secondary Education",
+    "m_clg_comp" : "Male College Completion",
+    "pri_edu" : "Primary Education",
+    "ls_edu" : "Lower Secondary Education",
+    "hs_edu" : "Higher Secondary Education",
+    "clg_comp" : "College Completion",
+    "gdp" : "GDP per Capita",
+    "le" : "Life Expectancy",
+    "tfr" : "Total Fertility Rate",
+    "time" : "Years",
+    
+}
+
+cleaned_indices_reversed = {v: k for k, v in cleaned_indices.items()}
+
+
+selected_options = []
+selected_ys = []
+
+params = st.experimental_get_query_params()
+selected_countries = params.get("c", countries)
+selected_countries = selected_countries[0].split(",")
+selected_options = params.get("gender", [])
+if(len(selected_options) == 0):
+    selected_options = ["Female"]
+print("selected_options",selected_options)
+selected_x, selected_y = indices[0], indices[1]
+try:
+    selected_x = cleaned_indices[params.get("x", indices)[0]]
+except:
+    pass
+
+try:
+    selected_y = cleaned_indices[params.get("y", indices)[0]]
+except:
+    pass
+
+try:
+    start_year = params.get("sy", 1960)[0]
+except:
+    start_year = 1960
+try:
+    end_year = params.get("ey", 2020)[0]
+except:
+    end_year = 2020
+
+
+col1, col2 = st.columns(2)
+
+selected_y      = col1.selectbox("Select y axis", indices, index=indices.index(selected_y))
+selected_x      = col2.selectbox("Select x axis", indices, index=indices.index(selected_x))
+
+
+if(selected_y in edu_indices):
+    options = ['Both', 'Male', 'Female']
+    print("selected_options",selected_options)
+    col1, col2, col3 = st.columns(3)
+
+    checkbox_state1 = col1.checkbox(options[0],value = options[0] in selected_options)
+    checkbox_state2 = col2.checkbox(options[1],value = options[1] in selected_options)
+    checkbox_state3 = col3.checkbox(options[2],value = options[2] in selected_options)
+
+    if checkbox_state1:
+        selected_options.append(options[0])
+    else:
+        selected_options = [option for option in selected_options if option != options[0]]
+    if checkbox_state2:
+        selected_options.append(options[1])
+    else:
+        selected_options = [option for option in selected_options if option != options[1]]
+    if checkbox_state3:
+        selected_options.append(options[2])
+    else:
+        selected_options = [option for option in selected_options if option != options[2]]
+
+    selected_options = list(set(selected_options))
+    for selected_option in selected_options:
+        selected_ys.append((selected_option + " " + selected_y,selected_option))
+    st.write(selected_y)
+else:
+    selected_ys.append((selected_y,""))
+    
+if(selected_x in edu_indices):
+    selected_x = "Both " + selected_x
+# Add a dropdown box to select a country
+selected_countries = st.multiselect("Select Countries", countries, selected_countries)
+
+selected_years  = st.slider("Select years", 1960, 2020, (int(start_year), int(end_year)))
+
+
+
+st.experimental_set_query_params(
+    c=",".join(selected_countries),
+    x=cleaned_indices_reversed[selected_x],
+    y=cleaned_indices_reversed[selected_y],
+    sy=selected_years[0],
+    ey=selected_years[1],
+    gender=selected_options
+)
+
+
+# plot the line chart using Matplotlib
+fig, ax = plt.subplots()
+country_coords = None
+for selected_country in selected_countries:
+    for selected_y,gender in selected_ys:
+        country_coords = get_country_coords(selected_country, selected_x, selected_y,selected_years)
+        ax.plot(country_coords["x"], country_coords["y"], label=selected_country + " " + gender)
+
+ax.set_xlabel(selected_x)
+ax.set_ylabel(selected_y)
+ax.set_title(f"{selected_y} vs {selected_x}")
+ax.legend()
+
+if country_coords is not None:
+    col1, col2  = st.columns(2)
+    file_name   = f"{selected_x[:3]}_{selected_y[:3]}"
+
+    csv_snippet = save_csv(selected_countries, selected_x, selected_y, selected_years)
+    with open("chart.csv", "rb") as f:
+        data_bytes = f.read()
+        col1.download_button(
+            label       = "Download Data 💿",
+            data        = data_bytes,
+            file_name   = f"{file_name}.csv",
+            mime        = "text/csv",
+        )
+
+    fig.savefig("chart.png")
+    with open("chart.png", "rb") as f:
+        image_bytes = f.read()
+        col2.download_button(
+            label       = "Download Graph 📈",
+            data        = image_bytes,
+            file_name   = f"{file_name}.png",
+            mime        = "image/png",
+        )
+# display the chart in Streamlit app
+st.pyplot(fig)
+
+
+st.markdown(
+"""**_Note_** :  the Education data used is only 20-25 year old age group and data and it is as follows:
+
+- **Primary Education**           : 6 years of education
+- **Lower Secondary Education**   : 9 years of education
+- **Higher Secondary Education**  : 12 years of education
+- **College Completion**          : 16 years of education
+                        """
+)
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,80 @@
+# pyright: reportMissingModuleSource=false
+import pandas as pd
+
+root = "./edu_datasets/"
+datasets_path = {
+    "Female Primary Education": root + "20-24-female-Primary_fin.csv",
+    "Female Lower Secondary Education": root + "20-24-female-Lower_Secondary_fin.csv",
+    "Female Higher Secondary Education": root + "20-24-female-Higher_Secondary_fin.csv",
+    "Female College Completion": root + "20-24-female-College_comp.csv",
+    
+    "Male Primary Education": root + "20-24-male-Primary_fin.csv",
+    "Male Lower Secondary Education": root + "20-24-male-Lower_Secondary_fin.csv",
+    "Male Higher Secondary Education": root + "20-24-male-Higher_Secondary_fin.csv",
+    "Male College Completion": root + "20-24-male-College_comp.csv",
+    
+    "Both Primary Education": root + "20-24-Primary_fin.csv",
+    "Both Lower Secondary Education": root + "20-24-Lower_Secondary_fin.csv",
+    "Both Higher Secondary Education": root + "20-24-Higher_Secondary_fin.csv",
+    "Both College Completion": root + "20-24-College_comp.csv",
+    
+    "Total Fertility Rate": root + "children_per_woman_total_fertility.csv",
+    "Life Expectancy": root + "life_expectancy_years.csv",
+    
+    "GDP per Capita": root + "gdppercapita_us_inflation_adjusted.csv",
+    
+    "Years": root + "years.csv",
+    
+    "india_tfr": root + "India/TFR.csv",
+}
+
+
+def get_country_coords(country, x, y, years):
+    df_1 = pd.read_csv(datasets_path[x])
+    df_2 = pd.read_csv(datasets_path[y])
+
+    df_1 = df_1[df_1["Country"] == country]
+    df_2 = df_2[df_2["Country"] == country]
+
+    df_1 = df_1.drop(["Country"], axis=1)
+    df_2 = df_2.drop(["Country"], axis=1)
+
+    df_1 = df_1.T
+    df_2 = df_2.T
+
+    df_3 = df_1.merge(df_2, left_index=True, right_index=True)
+    df_3.columns = ["x", "y"]
+
+    df_3.reset_index(inplace=True)
+    df_3.rename(columns={"index": "year"}, inplace=True)
+    df_3 = df_3[df_3["year"].str.isnumeric()]
+    df_3["year"] = df_3["year"].astype(int)
+    df_3 = df_3[df_3["year"] >= years[0]]
+    df_3 = df_3[df_3["year"] <= years[1]]
+    df_3.dropna(inplace=True)
+
+    df_3.drop(["year"], axis=1, inplace=True)
+    return df_3
+
+
+def save_csv(selected_countries, x, y, years):
+    df_1 = pd.read_csv(datasets_path[x])
+    df_2 = pd.read_csv(datasets_path[y])
+    df_1 = df_1[df_1["Country"].isin(selected_countries)]
+    df_2 = df_2[df_2["Country"].isin(selected_countries)]
+
+    melted_df_1 = pd.melt(df_1, id_vars=["Country"], var_name="year", value_name=x)
+
+    melted_df_1["year"] = melted_df_1["year"].astype(int)
+
+    melted_df_2 = pd.melt(df_2, id_vars=["Country"], var_name="year", value_name=y)
+    melted_df_2["year"] = melted_df_2["year"].astype(int)
+
+    merged_df = pd.merge(melted_df_1, melted_df_2, on=["Country", "year"])
+    merged_df.sort_values(by=["Country", "year"], inplace=True)
+    
+    merged_df = merged_df[merged_df["year"] >= years[0]]
+    merged_df = merged_df[merged_df["year"] <= years[1]]
+    
+    merged_df.to_csv("chart.csv", index=False)
+    return
